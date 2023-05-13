c5eed9a1b97ed5e94a87c3ca4658db97506f0bf9
Everton Oliveira <tonn.oliveira2011@gmail.com>
2023-05-13 16:27:02 -0300

    feat: Adicionando Google Tag Manager

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['raw.githubusercontent.com']
+    domains: ['images.pexels.com', 'github.com', 'raw.githubusercontent.com']
   }
 }
 
